fb48c542bf36f982cfcfa53c9e69309c7c8eacb3
Yusuf <yusuf2013dream@gmail.com>
2018-03-03 23:28:52 -0500

    update lab2

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -7,17 +7,7 @@ import random
 import csv
 import gc
 
-class Time_out:
-    def __init__(self,time):
-        self.time = time
 
-class ACK:
-    def __init__(self,RN,status):
-        self.RN = RN
-        self.status = status
-
-    def changeStatus(self,status):
-        self.status = status
 class Event:
     def __init(self,itype,time,error_flag,sequence_number):
         self.type= itype
@@ -25,23 +15,105 @@ class Event:
         self.error_flag = error_flag
         self.sequence_number = sequence_number
 
+def mergeSort(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
 
-def ABPsender(H,l):
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i].time < righthalf[j].time:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    return alistz       
+
+def ABPsender(H,l,C):
+    # initialization
+    ES = []
     SN = 0
     next_expected_ack =(SN+1)%2
     TimeOutTime=100
     current_time = 0
-    # H is header length, l is packet length
     packetLength = H+l
-    addTimeOutEvent()
-    packet = getpacket()
-    send(SN,packet,t)
+    # H is header length, l is packet length
 
 
-def send(Time,SN,packet):
+    TimeOutEvent = current_time+packetLength/C+TimeOutTime
+    ES = addTimeOutEvent(ES,TimeOutEvent,SN)
+    result = send(SN,packetLength,current_time)
+    if(result.type != 'NIL'):
+        ES.append(result)
+        ES = mergeSort(ES)
+    while(ES):
+        i = ES[0]
+        ES.remove(i)
+        #TODO update current time
+        if(i.type == 'TimeOutEvent'):
+            TimeOutEvent = current_time+packetLength/C+TimeOutTime
+            ES = addTimeOutEvent(ES,TimeOutEvent,SN) 
+            result = send(SN,packetLength,current_time)
+            if(result.type != 'NIL'):
+                ES.append(result)
+                ES = mergeSort(ES)
+        else if (i.type == 'ACKEvent'):
+            #packet send and received correctly
+            if(i.error_flag == 0 and next_expected_ack == i.sequence_number):
+                if(SN<2):
+                    SN = SN + 1
+                else:
+                    SN = 0
+                next_expected_ack =(SN+1)%2
+                ES = clearTimeOutEvent(ES)
+                TimeOutEvent = current_time+packetLength/C+TimeOutTime
+                ES = addTimeOutEvent(ES,TimeOutEvent,SN) 
+                result = send(SN,packetLength,current_time)
+                if(result.type != 'NIL'):
+                    ES.append(result)
+                    ES = mergeSort(ES)    
+
+
+def clearTimeOutEvent(ES):
+    for i in ES:
+        if(i.type == 'TimeOutEvent'):
+            ES.remove(i)
+    
+    return ES
+            
+
+def addTimeOutEvent(ES,time,SN):
+    TimeOutEvent = Event('TimeOutEvent',time,0,SN)
+    ES.append(TimeOutEvent)
+    return mergeSort(ES)
+
+def send(Time,SN,packetLength):
     forwardChannel() # sender to recevier
     receiver()
     reverseChannel()# receiver to sender
+    ACKEvent = Event('ACKEvent',time,error_flag,SN)
+    return
+
+def forwardChannel():
+    
 
 
 
